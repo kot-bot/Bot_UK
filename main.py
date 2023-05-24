4385575b282c3527df09d5cb8c7a6370b886262e
kotop21 <vitiat.gom@gmail.com>
2023-05-24 23:51:13 +0300

    new file:   __pycache__/config.cpython-311.pyc modified:   main.py new file:   words.db

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 import discord
+import config
+import sqlite3
 from discord.ext import commands
 
+TOKEN = config.TOKEN
+
 # Создаем экземпляр бота
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Список символов для удаления
-symbols = ['Ё', 'ё', 'Ъ', 'ъ', 'Ы', 'ы', 'Э', 'э']
+banned = ["Ё", "ё", "Ъ", "ъ", "Ы", "ы", "Э", "э"]
 
-# Событие запуска бота
+# Устанавливаем соединение с базой данных
+connection = sqlite3.connect('words.db')
+cursor = connection.cursor()
+
+# Создаем таблицу, если она не существует
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS banned_words (
+        word TEXT PRIMARY KEY
+    )
+''')
+connection.commit()
+
 @bot.event
 async def on_ready():
-    print('Бот готов')
+    print(f'Logged in as {bot.user.name}')
 
-# Событие при получении сообщения
+
 @bot.event
 async def on_message(message):
-    content = message.content
-    if any(symbol in content for symbol in symbols):
-        await message.delete()  # Удаляем сообщение
-        await message.channel.send(f'{message.author.mention}, ваше сообщение было удалено, так как содержало запрещенные символы.')
+    if message.author == bot.user:
+        return
+
+    # Проверяем, если сообщение было отправлено в личные сообщения (ЛС)
+    if isinstance(message.channel, discord.DMChannel):
+        return
+
+    # Получаем список запрещенных слов из базы данных
+    cursor.execute('SELECT word FROM banned_words')
+    words_banned = [word[0] for word in cursor.fetchall()]
+
+    if any(phrase in message.content for phrase in banned or words_banned):
+        user = message.author
+        channel = await user.create_dm()
+
+        await channel.send(f'СЛАВА УКРАЇНІ!!! Видалене повідомлення `{message.content}`')
+        await message.delete()
+
     await bot.process_commands(message)
 
-# Команда для проверки работоспособности бота
+
 @bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+@commands.has_permissions(administrator=True)
+async def add_banned_word(ctx, word):
+    # Добавляем новое запрещенное слово в базу данных
+    cursor.execute('INSERT INTO banned_words (word) VALUES (?)', (word,))
+    connection.commit()
+    await ctx.send(f'Слово "{word}" добавлено в список запрещенных.')
 
 # Запускаем бота
-bot.run('YOUR_BOT_TOKEN')
+bot.run(TOKEN)
